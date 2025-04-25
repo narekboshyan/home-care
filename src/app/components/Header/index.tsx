f9b0decdb945a095d3c973fb588e75f1785a6fbc
Narek Boshyan <narek.boshyan_ext@autoscout24.com>
2025-04-25 23:50:02 +0400

    working on logo

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Menu, X } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -11,6 +12,12 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between mx-auto">
         <Link href="/" className="flex items-center gap-2 px-3">
+          <Image
+            src="/care-northridge.png"
+            width={36}
+            height={36}
+            alt="Northridge Boarding Care"
+          />
           <span className="text-xl font-bold text-blue-900">
             Northridge Boarding Care
           </span>
