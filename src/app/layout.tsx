fefe5292625d35061bce5d206c6ab8015410b451
Narek Boshyan <narek.boshyan_ext@autoscout24.com>
2025-04-28 22:12:25 +0400

    feat: adding google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KNZD8FD8');`,
+        }}
+      />
       {/* Google Tag Manager Script - Place in head */}
       <Script
         id="gtm-script"
