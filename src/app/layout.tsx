9a7588d0355c321ea9f8367ac00d60d921538353
Narek Boshyan <narek.boshyan_ext@autoscout24.com>
2025-04-30 00:06:45 +0400

    working on email restructure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Footer } from "./components/Footer";
 import { ThemeProvider } from "./components/theme";
 import Script from "next/script";
 import { Metadata } from "next";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -61,6 +62,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Google Tag Manager Script */}
       <Script
         id="gtm-script"
         strategy="afterInteractive"
@@ -72,18 +74,25 @@ export default function RootLayout({
           })(window,document,'script','dataLayer','GTM-KNZD8FD8');`,
         }}
       />
-      {/* Google Tag Manager Script - Place in head */}
+
+      {/* Facebook Pixel Script */}
       <Script
-        id="gtm-script"
+        id="facebook-pixel"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KNZD8FD8');`,
+          __html: `!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1816636735572084');
+          fbq('track', 'PageView');`,
         }}
       />
+
       <head>
         <link rel="icon" href="./favicon.ico" />
         <meta
@@ -93,13 +102,24 @@ export default function RootLayout({
       </head>
 
       <body className={inter.className}>
-        {/* Google Tag Manager noscript - Immediately after opening body tag */}
+        {/* Google Tag Manager noscript */}
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNZD8FD8"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
+
+        {/* Facebook Pixel noscript */}
+        <noscript>
+          <Image
+            alt="Facebook Pixel"
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1816636735572084&ev=PageView&noscript=1"
+          />
+        </noscript>
 
         <ThemeProvider
           attribute="class"
